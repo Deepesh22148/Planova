f46a4547a8b3f9d12cebdb5ff84ee5472d0f1726
Deepesh22148 <deepesh22148@iiitd.ac.in>
2025-08-05 23:24:54 +0530

    initial project - first commit

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginPage from "@/pages/LoginPage";
+
+const SignInPage = () => {
+    return (
+        <LoginPage />
+    )
+}
+
+export default SignInPage
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <div className={"bg-violet-900/30 h-screen w-screen flex items-center justify-center"}>
+            Login Page
+        </div>
+    )
+}
+
+export default LoginPage
